f0dba2ee13aa70d8b1bbdf64f2e3715fa6608b29
Ivan Erik Kragh <iek@kapacity.dk>
2021-07-28 15:28:59 +0200

    Update remove unique constraint.

--- SSMS/meta/Tables/SourceConnectionDataTypeMapping.sql
+++ SSMS/meta/Tables/SourceConnectionDataTypeMapping.sql
@@ -8,6 +8,5 @@
 ,	[TargetDataTypeLength] INT NULL
 ,	[TargetDataTypeScale] INT NULL
 ,	CONSTRAINT [PK_SourceConnectionDataTypeMapping] PRIMARY KEY CLUSTERED ([SourceConnectionDataTypeMappingID]) WITH (OPTIMIZE_FOR_SEQUENTIAL_KEY = ON)
-,	CONSTRAINT [UC_SourceConnectionDataTypeMapping] UNIQUE NONCLUSTERED ([SourceConnectionID], [SourceDataType])
 ,	CONSTRAINT [FK_SourceConnectionDataTypeMapping_SourceConnection] FOREIGN KEY([SourceConnectionID]) REFERENCES [meta].[SourceConnection] ([SourceConnectionID])
 )
