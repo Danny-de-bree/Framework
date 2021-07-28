9f5bc7cdc80229b93cd35555dd6008eefccd8d41
Ivan Erik Kragh <iek@kapacity.dk>
2021-07-28 15:02:21 +0200

    Add meta.SourceConnectionDataTypeMapping table to framework.

---
+++ SSMS/meta/Tables/SourceConnectionDataTypeMapping.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [meta].[SourceConnectionDataTypeMapping] (
+	[SourceConnectionDataTypeMappingID] BIGINT IDENTITY(1,1) NOT NULL
+,	[SourceConnectionID] BIGINT NOT NULL 
+,	[SourceDataType] NVARCHAR(255) NOT NULL
+,	[SourceDataTypeLength] INT NULL
+,	[SourceDataTypeScale] INT NULL
+,	[TargetDataType] NVARCHAR(255) NOT NULL
+,	[TargetDataTypeLength] INT NULL
+,	[TargetDataTypeScale] INT NULL
+,	CONSTRAINT [PK_SourceConnectionDataTypeMapping] PRIMARY KEY CLUSTERED ([SourceConnectionDataTypeMappingID]) WITH (OPTIMIZE_FOR_SEQUENTIAL_KEY = ON)
+,	CONSTRAINT [UC_SourceConnectionDataTypeMapping] UNIQUE NONCLUSTERED ([SourceConnectionID], [SourceDataType])
+,	CONSTRAINT [FK_SourceConnectionDataTypeMapping_SourceConnection] FOREIGN KEY([SourceConnectionID]) REFERENCES [meta].[SourceConnection] ([SourceConnectionID])
+)
